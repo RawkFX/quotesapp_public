3272b0498c0f5f2f5c494b1ed1d09ac12bae4187
Răzvan <razvanpirvan@hotmail.com>
2024-04-23 11:42:51 +0300

    :sparkles: Small Updates

--- backend/target/classes/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO quote (content, author) VALUES ('The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela'), ('The way to get started is to quit talking and begin doing.', 'Walt Disney');
